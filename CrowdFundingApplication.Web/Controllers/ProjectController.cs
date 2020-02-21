c45193e4b60aa26f9c70a2860be00d45c0dfd8d6
KCA9 <stafylaskonstantinos@gmail.com>
2020-02-21 16:17:14 +0200

    -Create html user services (extra html views)

---
+++ CrowdFundingApplication.Web/Controllers/ProjectController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CrowdFundingApplication.Web.Models;
+
+namespace CrowdFundingApplication.Web.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult AddProject()
+        {
+            return View();
+        }
+        public IActionResult ProjectList()
+        {
+            return View();
+        }
+    }
+}
